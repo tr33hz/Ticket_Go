11907a711e568e84a2c1f92a871e017efacfa1a1
trhz <treezmusic@yandex.ru>
2024-07-04 16:51:03 +0300

    init ticket-service, add Lombok, JDBC, driver PostgreSQL, Spring Web

---
+++ ticket-service/src/main/java/org/example/TicketService.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketService {
+    public static void main(String[] args) {
+        SpringApplication.run(TicketService.class, args);
+    }
+}
